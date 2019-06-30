59e0abb063cb6fdd20b745104e6d50d254177736
Beno Suriano <bjsuriano@gmail.com>
2019-06-29 19:10:12 -0600

    Added 404 functionality.

--- client/Main.js
+++ client/Main.js
@@ -6,6 +6,7 @@ import Authors from './Authors'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import SingleStory from './SingleStory'
 import SingleAuthor from './SingleAuthor'
+import NotFound from './NotFound'
 
 export default class Main extends React.Component {
   render () {
@@ -23,6 +24,7 @@ export default class Main extends React.Component {
           <Route exact path='/authors' component={Authors} />
           <Route path='/stories/:storyId' component={SingleStory} />
           <Route path='/authors/:authorId' component={SingleAuthor} />
+          <Route component={NotFound} />
         </div>
       </Router>
     )
